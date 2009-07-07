6a694bc42dbaaae56bebb65adc50ba5948480136
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-07 13:43:21 +0000

    adding version 1.0
    git-svn-id: http://plugins.svn.wordpress.org/comment-autogrow/trunk@132685 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ comment-autogrow.php
@@ -0,0 +1,57 @@
+<?php
+/*
+Plugin Name: Comment Autogrow
+Version: 1.0
+Description: Makes the comment textarea expand in height automatically
+Author: scribu
+Author URI: http://scribu.net
+Plugin URI: http://scribu.net/wordpress/comment-autogrow
+
+Copyright (C) 2009 scribu.net (scribu AT gmail DOT com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+commentAutogrow::init();
+
+class commentAutogrow
+{
+	function init()
+	{
+		add_action('template_redirect', array(__CLASS__, 'script'));
+	}
+
+	function script()
+	{
+		if ( ! is_single() || is_page() )
+			return;
+
+		$url = plugins_dir_url(__FILE__);
+
+		wp_register_script('autogrow', $url . 'autogrow.js', array('jquery'), '1.2.4', true);
+		wp_enqueue_script('comment-autogrow', $url . 'init.js', array('autogrow'), '1.0', true);
+	}
+}
+
+// WP < 2.8
+if ( !function_exists('plugins_dir_url') ) :
+function plugins_dir_url($file) {
+	// WP < 2.7
+	if ( !function_exists('plugins_url') )
+		return trailingslashit(get_option('siteurl') . '/wp-content/plugins/' . plugin_basename($file));
+
+	return trailingslashit(plugins_url(plugin_basename(dirname($file))));
+}
+endif;
+
---
+++ readme.txt
@@ -0,0 +1,39 @@
+=== Comment Autogrow ===
+Contributors: scribu
+Donate link: http://scribu.net/wordpress
+Tags: comments, javascript, autogrow
+Requires at least: 2.3
+Tested up to: 2.8
+Stable tag: trunk
+
+Makes the comment textarea expand in height automatically
+
+== Description ==
+
+Getting comments on your site is one of the most important goals. Long comments tend to be especially valuable.
+
+However, most comment textareas have a small height, making it difficult to write long comments.
+
+*Enter Autogrow*
+
+Autogrow is a small piece of JavaScript, originally written by [Chrys Bader](http://www.chrysbader.com).
+
+Here's how it works: When a commenter almost fills the textarea, instead of getting a scrollbar, the textarea grows in height.
+
+This means (s)he can see the entire comment while it's being written, no matter how long.
+
+You can try it for yourself on the [Plugin's homepage](http://scribu.net/wordpress/comment-autogrow/ca-1-0.html#respond).
+
+== Installation ==
+
+You can either install it automatically from the WordPress admin, or do it manually:
+
+1. Unzip "comment-autogrow" archive and put the folder into your plugins folder (/wp-content/plugins/).
+1. Activate the plugin from the Plugins menu.
+
+== Changelog ==
+
+= 1.0 =
+* initial release
+* [more info](http://scribu.net/wordpress/comment-autogrow/ca-1-0.html)
+
